2d471c9273b7bf8b4234363e4bad37d94d252b09
chengkev <chengkev2000@gmail.com>
2023-01-24 13:22:10 -0400

    fix: restore pages and component file location

--- src/app/App.js
+++ src/app/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import "../MenuAdmin/index.scss";
-import Sidebar from "../MenuAdmin/Sidebar";
-import SidebarAgente from '../MenuAgente/SidebarAgente';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import "../components/MenuAdmin/index.scss";
+import { HomePage } from '../pages/Home'
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() { 
   return (
-    <div className="App">
-    
-    <Sidebar/>   
-    </div>  
+      <BrowserRouter>
+        <Route exact path="/" component={HomePage}/>
+      </BrowserRouter>
     );
 }
 export default App;
