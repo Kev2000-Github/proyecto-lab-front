496b3ec52b999e9ffb83e4565a55c10d05a0f62c
luiger <luiger852@gmail.com>
2023-01-24 12:19:12 -0400

    agregado los sidebar

---
+++ src/MenuAgente/SidebarDataAgente.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./agente.scss";
+import LogoutIcon from '@mui/icons-material/Logout';
+
+
+export const SidebarDataAgente = [
+    {    
+        title: "Logout",
+        icon: <LogoutIcon />,
+        link: "/logout"
+    }
+]
